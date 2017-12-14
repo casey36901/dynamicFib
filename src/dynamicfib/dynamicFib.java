6f8755bc57292bb414a1604ecabd596a9b20477e
casey36901 <casey369@morris.umn.edu>
2017-12-13 18:06:42 -0600

    Finishes dynamicFib, doesn't work for more than max value of long

--- src/dynamicfib/dynamicFib.java
+++ src/dynamicfib/dynamicFib.java
@@ -5,8 +5,8 @@ public class dynamicFib {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		fibs[0] = 1;
 		fibs[1] = 1;
+		fibs[2] = 1;
 		
 		if(args == null){
 			System.out.println("Please enter a number to compute");
@@ -16,26 +16,18 @@ public class dynamicFib {
 			
 			Integer Fib = Integer.parseInt(args[0]);
 			System.out.println(findFib(Fib));
-			
 		}
-		
 	}
 		 
 		 public static long findFib(Integer n){
 			 long result;
-			// System.out.println(fibs[100]);
-//			 if(n <= 2){
-//				 return 1;
-//			 }
+			
 			if(fibs[n] != 0){
 				result = fibs[n];
-				System.out.println("I used the array to calc result!");
 					}
 					else{
-						System.out.println("I calced the result the slow way");
 						result = findFib(n-1)+findFib(n-2);
 						fibs[n] = result;
-						//System.out.println("fibs value" + fibs[n]);
 					}
 			return result;
 				}
